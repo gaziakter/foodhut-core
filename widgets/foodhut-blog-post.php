156cc06e9da0b01013f8aa411e22d3917dd5a6c8
Gazi Akter <gaziakterdev@gmail.com>
2024-05-30 22:25:39 +0600

    blog post content complated

--- widgets/foodhut-blog-post.php
+++ widgets/foodhut-blog-post.php
@@ -222,13 +222,13 @@ class Foodhut_Blog_Post extends Widget_Base {
 
 					<?php if ( $query->have_posts() ) : ?>
 						<?php while ( $query->have_posts() ) : $query->the_post();
-						$categories = get_the_category(get_the_ID());
+						$price = get_post_meta(get_the_ID(), 'price', true);
 						?>
                     <div class="col-md-4 my-3 my-md-0">
                         <a href="<?php the_permalink(); ?>"class="card bg-transparent border">
 							<?php the_post_thumbnail(); ?>
                             <div class="card-body">
-                                <h1 class="text-center mb-4"><a href="#" class="badge badge-primary">$5</a></h1>
+                                <h1 class="text-center mb-4"><a href="<?php the_title(); ?>" class="badge badge-primary"><?php echo esc_html($price); ?></a></h1>
                                 <h4 class="pt20 pb20"><?php the_title(); ?></h4>
                                 <p class="text-white"><?php the_excerpt(); ?></p>
                             </div>
